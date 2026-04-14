85b199c91fed3433d95308fca23166f00063e2f3
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-04-14 17:57:40 +0200

    Create Gifs side menu header

--- src/app/gifs/pages/dashboard-page/dashboard-page.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuHeader } from "../../components/side-menu-header/side-menu-header";
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenuHeader],
   standalone: true,
   templateUrl: './dashboard-page.html',
 })
