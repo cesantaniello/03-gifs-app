098be6ba1f88e0f05d815190603281aef4c66d37
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-04-14 14:45:17 +0200

    Children routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,29 @@
 import { Routes } from '@angular/router';
 import DashboardPageComponent from './gifs/pages/dashboard-page/dashboard-page';
+import TrendingPageComponent from './gifs/pages/trending-page/trending-page';
+import SearchPageComponent from './gifs/pages/search-page/search-page';
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full'
+    path: 'dashboard',
+    component: DashboardPageComponent,
+    children: [
+      {
+        path: 'trending',
+        component: TrendingPageComponent,
+      },
+      {
+        path: 'search',
+        component: SearchPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'trending',
+      },
+    ],
   },
   {
-    path: 'dashboard',
-    component: DashboardPageComponent
-  }
+    path: '**',
+    redirectTo: 'dashboard',
+  },
 ];
--- src/app/gifs/pages/dashboard-page/dashboard-page.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './dashboard-page.html',
 })
 export default class DashboardPageComponent { }
