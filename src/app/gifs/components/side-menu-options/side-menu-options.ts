ddc5e3b6e00ac90ad9fa243fae5bce37c47b630d
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-04-16 11:49:51 +0200

    Create side menu options component

--- src/app/gifs/components/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu-options/side-menu-options.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
 
+interface MenuOption {
+  icon: string;
+  label: string;
+  subLabel: string;
+  route: string;
+}
+
 @Component({
   selector: 'gifs-side-menu-options',
   imports: [],
   templateUrl: './side-menu-options.html',
 })
-export class SideMenuOptions { }
+export class SideMenuOptions {
+  menuOptions: MenuOption[] =
+  [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs populares',
+      route: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    },
+  ];
+}
